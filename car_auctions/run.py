790a7210b4fa8f5268b3d8386c332f4d46e3a391
Daniel Zhang <dzhanner17@gmail.com>
2020-06-29 01:40:38 -0400

    parse models and refactors

--- car_auctions/run.py
+++ car_auctions/run.py
@@ -1,9 +1,9 @@
-import time
+import re
 from collections import defaultdict
 from typing import Dict, List, Optional, Set
 
-import pandas as pd
 import numpy as np
+import pandas as pd
 
 from car_auctions import utils
 
@@ -33,58 +33,43 @@ def get_models_by_makes(car_makes_file_path: str, car_models_file_path: str) -> 
     return models_by_makes
 
 
-def get_car_makes(car_makes_file_path: str) -> pd.Series:
-    utils.validate_file_path(car_makes_file_path)
-
-    makes_df = pd.read_csv(car_makes_file_path)
-    makes_df['make'] = makes_df['make'].str.lower().str.replace(r'[^a-zA-Z0-9]', '')
+def get_car_makes(car_makes_file_path: str) -> pd.DataFrame:
+    makes_df = utils.read_csv_to_lower_case(car_makes_file_path)
+    makes_df['make'] = makes_df['make'].str.replace(r'[^a-zA-Z0-9]', '')
     return makes_df
 
 
-def get_car_models(car_models_file_path: str) -> pd.Series:
-    utils.validate_file_path(car_models_file_path)
+def get_car_models(car_models_file_path: str) -> pd.DataFrame:
+    makes_and_models_df = utils.read_csv_to_lower_case(car_models_file_path)
 
-    makes_and_models_df = pd.read_csv(car_models_file_path)
-    makes_and_models_df['make'] = makes_and_models_df['make'].str.lower().str.replace(r'[^a-zA-Z0-9]', '')
-    makes_and_models_df['model'] = makes_and_models_df['model'].str.lower().str.replace(r'[^a-zA-Z0-9 ]', '')
+    makes_and_models_df['make'] = makes_and_models_df['make'].str.replace(r'[^a-zA-Z0-9]', '')
+    makes_and_models_df['model'] = makes_and_models_df['model'].str.replace(r'[^a-zA-Z0-9]', '')
 
     return makes_and_models_df
 
 
-def build_car_auction_dataframe(car_auctions_file_path: str, car_makes: Dict[str, Set]) -> pd.DataFrame:
-    utils.validate_file_path(car_auctions_file_path)
-    main_df = pd.read_csv(car_auctions_file_path)
-
-    # populate mileage
-    main_df['Mileage'] = extract_mileage_series(main_df)
-
-    # parse description_url
+def build_car_auction_dataframe(main_df: pd.DataFrame, models_by_makes: Dict[str, Set]) -> pd.DataFrame:
     url = main_df['description_url'].str.extract('https://bringatrailer.com/listing/(.+)/$', expand=False)
     url_components = url.str.split('-', expand=False)
 
-    # populate year
-    main_df['Year'] = extract_year_series(url_components, main_df)
-    # print(main_df['Year'].isnull().sum())
-
-    # populate make
-    main_df['Make'] = extract_make_series(url_components, main_df, car_makes)
-    # print(main_df['Make'].isnull().sum())
-
-    # populate no reserve
-    main_df['No Reserve'] = extract_no_reserve_series(main_df)
+    main_df['year'] = extract_year_series(url_components, main_df)
+    main_df['make'] = extract_make_series(url_components, main_df, models_by_makes)
+    main_df['model'] = extract_model_series(main_df, models_by_makes)  # *run this after extracting both Year and Make*
+    main_df['no reserve'] = extract_no_reserve_series(main_df)
+    main_df['mileage'] = extract_mileage_series(main_df)
 
     return main_df
 
 
 def extract_mileage_series(main_df: pd.DataFrame) -> pd.Series:
-    original_mileage = main_df['description_name'].str.extract(r'([0-9k,]+)-Mile', expand=False)
+    original_mileage = main_df['description_name'].str.extract(r'([0-9k,]+)-mile', expand=False, flags=re.IGNORECASE)
     cleaned_mileage = original_mileage.str.replace(',', '').str.replace('k', '000')
 
     return cleaned_mileage
 
 
 def extract_year_series(url_components: pd.Series, main_df: pd.DataFrame) -> pd.Series:
-    # get years from the description_url column
+    # get years from the description_url column first
     years = pd.to_numeric(url_components.str[0], errors='coerce', downcast='integer')
     invalid_years_bool_idx = np.logical_or.reduce([years.isnull(), years < 1900, years > 2039])
 
@@ -93,46 +78,92 @@ def extract_year_series(url_components: pd.Series, main_df: pd.DataFrame) -> pd.
     matched_years = main_df.loc[invalid_years_bool_idx, 'description_name'].str.extract(valid_year_regex, expand=False)
 
     years[invalid_years_bool_idx] = matched_years
-    return years
+    years_int = years.fillna(0).astype('int')
+    years_int.loc[years_int == 0] = ''
+
+    return years_int
 
 
-def extract_make_series(url_components: pd.Series, main_df: pd.DataFrame, car_makes: Dict[str, Set]) -> pd.Series:
-    # get makes from the description_url column
-    makes = url_components.apply(lambda row: _filter_by_known_makes(row, car_makes))
+def extract_make_series(url_components: pd.Series, main_df: pd.DataFrame, models_by_makes: Dict[str, Set]) -> pd.Series:
+    # get makes from the description_url column first
+    makes = url_components.apply(lambda row: _filter_by_known_makes(row, models_by_makes))
     missing_makes_bool_idx = makes.isnull()
 
     # try to get the remaining missing makes from the description_name column
     matched_makes = main_df.loc[missing_makes_bool_idx].apply(
-        lambda row: _filter_by_known_makes(row['description_name'].split(' '), car_makes), axis=1)
+        lambda row: _filter_by_known_makes(row['description_name'].split(' '), models_by_makes), axis=1)
 
     makes[missing_makes_bool_idx] = matched_makes
     return makes
 
 
 def extract_no_reserve_series(main_df: pd.DataFrame) -> pd.Series:
-    no_reserve: pd.Series = main_df['description_name'].str.extract(r'(No Reserve):', expand=False)
-    no_reserve.replace(['No Reserve', np.NaN], ['Yes', 'No'], inplace=True)
+    no_reserve = main_df['description_name'].str.extract(r'(no reserve|reseve):', expand=False, flags=re.IGNORECASE)
+    no_reserve.replace(['no reserve', 'no reseve', np.NaN], ['yes', 'yes', 'no'], inplace=True)
 
     return no_reserve
 
 
-def _filter_by_known_makes(column_val: Optional[List], car_makes: Dict[str, Set]) -> Optional[str]:
+def extract_model_series(main_df: pd.DataFrame, models_by_makes: Dict[str, Set]) -> pd.Series:
+    main_df['description_name'] = main_df['description_name'].str.replace('-', ' ')
+    models = main_df.apply(lambda row: _filter_by_known_models(row, models_by_makes), axis=1)
+
+    return models
+
+
+def _filter_by_known_makes(column_val: Optional[List], models_by_makes: Dict[str, Set]) -> Optional[str]:
     if isinstance(column_val, list):
         column_val_without_nums = [col_part for col_part in column_val if not col_part.isnumeric()]
         column_substrings = utils.create_substrings_from_list_of_strings(column_val_without_nums)
 
         for col_substring in column_substrings:
-            if col_substring in car_makes:
+            if col_substring in models_by_makes:
                 return col_substring
 
     return pd.NA
 
 
-if __name__ == '__main__':
-    a = time.time()
+def _filter_by_known_models(row: pd.Series, models_by_makes: Dict[str, Set]) -> Optional[str]:
+    # description_name = _strip_description_name(row)
+    description_name = row['description_name']
+    description_name_list = description_name.split()
+    desciption_name_substrings = utils.create_substrings_from_list_of_strings(description_name_list)
 
+    make = row['make']
+    if make in models_by_makes:
+        models_for_make = models_by_makes[make]
+        for potential_model_substring in desciption_name_substrings:
+            if potential_model_substring in models_for_make:
+                return potential_model_substring
+
+    return pd.NA
+
+
+def _strip_description_name(row):
+    description_name = row['description_name']
+    year = str(row['year'])
+    make = str(row['make'])
+
+    description_name = re.sub(year, '', description_name)
+    description_name = re.sub(make, '', description_name, flags=re.IGNORECASE)
+
+    description_name = re.sub(r'no (?:reserve: |reseve: )', '', description_name, flags=re.IGNORECASE)
+    description_name = re.sub(r'(?:[0-9k,]+)-mile', '', description_name, flags=re.IGNORECASE)
+    description_name = re.sub(r' {2,}', ' ', description_name)
+
+    description_name = description_name.strip()
+
+    return description_name
+
+
+@utils.timer
+def main():
     models_by_makes = get_models_by_makes('../car_makes.csv', '../car_models.csv')
-    car_auction_data = build_car_auction_dataframe('../bring_a_trailer.csv', models_by_makes)
+    car_auctions_df = utils.read_csv_to_lower_case('../copy_bring_a_trailer.csv')
+    car_auction_data = build_car_auction_dataframe(car_auctions_df, models_by_makes)
 
-    b = time.time()
-    print(b - a)
+    return car_auction_data
+
+
+if __name__ == '__main__':
+    main()
--- car_auctions/utils.py
+++ car_auctions/utils.py
@@ -1,10 +1,38 @@
+import functools
 import os
+import time
 from typing import List
+
+import pandas as pd
+
+
+def timer(func):
+    @functools.wraps(func)
+    def wrapper_timer(*args, **kwargs):
+        tic = time.perf_counter()
+        value = func(*args, **kwargs)
+        toc = time.perf_counter()
+        elapsed_time = toc - tic
+        print(f"Elapsed time: {elapsed_time:0.4f} seconds")
+        return value
+
+    return wrapper_timer
 
 
 def validate_file_path(file_path: str) -> None:
     if not os.path.isfile(file_path):
-        raise FileNotFoundError(f'The file_path {file_path} does not exist')
+        raise FileNotFoundError(f'The csv file_path {file_path} does not exist')
+
+
+def read_csv_to_lower_case(csv_file_path: str) -> pd.DataFrame:
+    validate_file_path(csv_file_path)
+
+    csv_df = pd.read_csv(csv_file_path)
+    csv_df.columns = [column_name.lower() for column_name in csv_df.columns]
+    # csv_df = csv_df.apply(lambda x: x.astype(str).str.lower())
+    csv_df = csv_df.applymap(lambda s: s.lower() if type(s) == str else s)
+
+    return csv_df
 
 
 def create_substrings_from_list_of_strings(list_of_strings: List[str]) -> List[str]:
